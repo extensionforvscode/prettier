048929abb69f8d222628e97b0cd68596bf2bc527
extensionforvscode <166860216+extensionforvscode@users.noreply.github.com>
2024-04-25 11:46:31 +0530

    Update MISSIONARIY-CANNIBALS.py

--- MISSIONARIY-CANNIBALS.py
+++ MISSIONARIY-CANNIBALS.py
@@ -1,51 +1,58 @@
 import heapq
 
-def isValid(state):
-    M, C, B = state
-    if M < 0 or C < 0 or M > 3 or C > 3:
+
+def isvalid(state):
+    m, c, b = state  # number of missionaries, cannibals, and boat position
+    if m < 0 or c < 0 or m > 3 or c > 3:  # missionaries or cannibals cannot be negative or greater than 3
         return False
-    if C > M and M > 0:
+    if c > m > 0:  # more cannibals than missionaries on the left side
         return False
-    if (3 - C) > (3 - M) and (3 - M) > 0:
+    if (3 - c > 3 - m) and (3 - m > 0):  # more cannibals than missionaries on the right side
         return False
     return True
 
-def successors(state):
-    M, C, B = state
+
+def successor(state):
+    m, c, b = state
     moves = []
-    if B == 1:
-        if isValid((M, C - 2, 0)):
-            moves.append((M, C - 2, 0))
-        if isValid((M - 2, C, 0)):
-            moves.append((M - 2, C, 0))
-        if isValid((M - 1, C - 1, 0)):
-            moves.append((M - 1, C - 1, 0))
-        if isValid((M, C - 1, 0)):
-            moves.append((M, C - 1, 0))
-        if isValid((M - 1, C, 0)):
-            moves.append((M - 1, C, 0))
-    else:
-        if isValid((M, C + 2, 1)):
-            moves.append((M, C + 2, 1))
-        if isValid((M + 2, C, 1)):
-            moves.append((M + 2, C, 1))
-        if isValid((M + 1, C + 1, 1)):
-            moves.append((M + 1, C + 1, 1))
-        if isValid((M, C + 1, 1)):
-            moves.append((M, C + 1, 1))
-        if isValid((M + 1, C, 1)):
-            moves.append((M + 1, C, 1))
+    if b == 1:  # boat on the left side
+        if isvalid((m, c - 2, 0)):
+            moves.append((m, c - 2, 0))
+        if isvalid((m - 2, c, 0)):
+            moves.append((m - 2, c, 0))
+        if isvalid((m - 1, c - 1, 0)):
+            moves.append((m - 1, c - 1, 0))
+        if isvalid((m, c - 1, 0)):
+            moves.append((m, c - 1, 0))
+        if isvalid((m - 1, c, 0)):
+            moves.append((m - 1, c, 0))
+    else:  # boat on the right side
+        if isvalid((m, c + 2, 1)):
+            moves.append((m, c + 2, 1))
+        if isvalid((m + 2, c, 1)):
+            moves.append((m + 2, c, 1))
+        if isvalid((m + 1, c + 1, 1)):
+            moves.append((m + 1, c + 1, 1))
+        if isvalid((m, c + 1, 1)):
+            moves.append((m, c + 1, 1))
+        if isvalid((m + 1, c, 1)):
+            moves.append((m + 1, c, 1))
     return moves
 
+
 def heuristic(state):
-    M, C, B = state
-    return (M + C - 2)//2
+    m, c, _ = state
+    return (m + c - 2) // 2
+
 
 def astar(start_state):
     heap = []
     heapq.heappush(heap, (heuristic(start_state), 0, [start_state]))
     visited = set()
+    i = 0
     while heap:
+        i += 1
+        print("Step " + str(i) + ": " + str(heap[-1][2]))
         _, cost, path = heapq.heappop(heap)
         current_state = path[-1]
         if current_state in visited:
@@ -53,44 +60,18 @@ def astar(start_state):
         if current_state == (0, 0, 0):
             return path
         visited.add(current_state)
-        for child in successors(current_state):
+        for child in successor(current_state):
             if child not in visited:
                 new_path = path + [child]
                 new_cost = cost + 1
                 heapq.heappush(heap, (new_cost + heuristic(child), new_cost, new_path))
     return None
 
-def print_actions(path):
-    for i in range(1, len(path)):
-        prev_state = path[i-1]
-        current_state = path[i]
-        move = (current_state[0] - prev_state[0], current_state[1] - prev_state[1], current_state[2] - prev_state[2])
-        if move[2] == 1:
-            boat_side = "left"
-        else:
-            boat_side = "right"
-        missionaries = abs(move[0])
-        cannibals = abs(move[1])
-        if missionaries == 0:
-            if cannibals == 1:
-                print(f"Move 1 cannibal to the {boat_side} side")
-            else:
-                print(f"Move {cannibals} cannibals to the {boat_side} side")
-        elif cannibals == 0:
-            if missionaries == 1:
-                print(f"Move 1 missionary to the {boat_side} side")
-            else:
-                print(f"Move {missionaries} missionaries to the {boat_side} side")
-        else:
-            print(f"Move {missionaries} missionaries and {cannibals} cannibals to the {boat_side} side")
 
-start_state = 3, 3, 1
+start_state = (3, 3, 1)
 solution = astar(start_state)
 if solution:
-    print("Solution found!")
-    print_actions(solution)
-
-    print("Everyone reached to other side of the bank :-) ")
+    print(solution)
+    print('Solution found.')
 else:
-    print("No solution found.")
-
+    print("No solution found")
